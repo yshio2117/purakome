8a2d516e800c298bd201e0e39400be9d583a4225
yusuke <yshio2117@gmail.com>
2023-05-06 20:36:02 +0900

    最近の投稿表示機能追加

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,9 @@ class Blog(models.Model):
     created_at = models.DateTimeField(verbose_name='作成日時',auto_now_add=True)
     update_at = models.DateTimeField(verbose_name='更新日時',auto_now=True)
 
+    SELECTION = ((0,'その他'),(1,'Tech'),(2,'OpEd'))
+    category = models.PositiveSmallIntegerField(verbose_name='カテゴリー',choices=SELECTION,default=0)
+
     
     class Meta:
         verbose_name_plural = 'Blog'
--- blog/views.py
+++ blog/views.py
@@ -8,21 +8,25 @@ class IndexView(generic.TemplateView):
     
 def tech_detail(request,blog_id):
     
+       
     if request.method == 'POST':
         
         blog = Blog.objects.get(id=blog_id)
+        latest_article = Blog.objects.filter(category=blog.category).order_by('created_at').reverse()[:5]
         params = {
             'blog':blog,        
             'title_tag':blog.title_tag.split(',') if blog.title_tag else None,
-
+            'latest_article':latest_article,
             }
     elif request.method == 'GET':
         blog = Blog.objects.get(id=blog_id)
-        print(blog.title_tag)
+        #print(blog.title_tag)
+        latest_article = Blog.objects.filter(category=blog.category).order_by('created_at').reverse()[:5]
         params = {
             'blog':blog,        
             'title_tag':blog.title_tag.split(',') if blog.title_tag else None,
+            'latest_article':latest_article,
             }
-        
+
     return render(request, 'tech_detail.html', params)        
 
